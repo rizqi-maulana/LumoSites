dfa7f767e21697c5c76dea70af122ecf406345a5
Maulanya <maulanarizq26@gmail.com>
2024-05-25 19:38:11 +0800

    Updated Tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,11 +11,6 @@ const svgToDataUri = require("mini-svg-data-uri");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-
-    // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   darkMode: "selector",
